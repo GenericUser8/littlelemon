d125c68ecb0ce04db1e05252d706112147724463
GenericUser8 <136227563+GenericUser8@users.noreply.github.com>
2024-10-02 13:54:11 +0800

    Setting up the XCode project.

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by MainUser on 2/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
